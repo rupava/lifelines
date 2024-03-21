5fb4536134dc0e060116663568d9c18ef1f47c46
rupava <rupava.baruah@gmail.com>
2024-03-22 02:22:40 +0530

    calender added

---
+++ static/js/main.js
@@ -0,0 +1,31 @@
+ function entryOpen(entryDate){
+    let entDate = new Date(entryDate);entDate.setHours(0,0,0,0);
+    let todayDate = new Date();todayDate.setHours(0,0,0,0);
+    if(entDate > todayDate){
+        alert("Can't make an entry for a future date.");
+        return false;
+    }
+    window.location.href = `/dashboard/entry/12/${entryDate}`;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    dayHeaderFormat: { weekday: 'short' },
+    headerToolbar: {
+    start: 'title',
+    center: '',
+    end: 'today prev,next,dayGridMonth,multiMonthYear',
+    },
+    dayHeaderClassNames: function(arg) {
+        return ['custom-day-header'];
+    },
+    dateClick: function(info) {
+        entryOpen(info.dateStr);
+    },
+    });
+    
+    
+  calendar.render();
+});
